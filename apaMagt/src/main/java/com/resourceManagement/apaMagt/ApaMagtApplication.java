8ab47453b2c649845240f0c18926da5cc1e7fbe8
Suresh <sureshqr@gmail.com>
2022-11-20 21:46:27 +0530

    Add files via upload

---
+++ apaMagt/src/main/java/com/resourceManagement/apaMagt/ApaMagtApplication.java
@@ -0,0 +1,13 @@
+package com.resourceManagement.apaMagt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApaMagtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApaMagtApplication.class, args);
+	}
+
+}
---
+++ apaMagt/src/test/java/com/resourceManagement/apaMagt/ApaMagtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resourceManagement.apaMagt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApaMagtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
