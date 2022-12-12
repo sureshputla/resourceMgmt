ebeb40ac2ea390d7568c25da9db06adab889d1de
Suresh <sureshqr@gmail.com>
2022-12-12 10:19:44 +0530

    added id in TaskDTO

--- apaMagt/src/main/java/com/resourcemanagement/apamanagement/model/TaskDTO.java
+++ apaMagt/src/main/java/com/resourcemanagement/apamanagement/model/TaskDTO.java
@@ -2,7 +2,17 @@ package com.resourcemanagement.apamanagement.model;
 
 
 public class TaskDTO {
+
+    private Integer id;
     private String taskName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getTaskName() {
         return taskName;
